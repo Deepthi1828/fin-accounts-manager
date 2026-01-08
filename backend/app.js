33c9278c4f96680e176284427bad1a3c603cc150
Deepthi1828 <239X1A05L7@gprec.ac.in>
2026-01-08 14:44:14 +0530

    Updated app.js to use environment port

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(require("cors")());
 
 app.get("/", (req, res) => {
   res.send("Finance Accounts Management API running");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
